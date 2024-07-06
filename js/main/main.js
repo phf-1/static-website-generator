e428dde89a95514cd30415370458b471b51f5d64
Pierre-Henry Fröhring <contact@phfrohring.com>
2024-07-06 11:49:45 +0200

    designer: use variable fonts.

--- js/main/main.js
+++ js/main/main.js
@@ -6,26 +6,26 @@
 
 // Position the toc in the page.
 const position_toc = function (main, content, toc) {
-  main.appendChild(toc);
-  const content_rect = content.getBoundingClientRect();
-  const content_children = Array.from(content.children);
-  const h1 = content_children.find(is_h1);
-  if (h1) {
-    const h1_rect = h1.getBoundingClientRect();
-    const h1_dy = h1_rect.y - content_rect.y;
-    const toc_top_margin = h1_dy + h1.offsetHeight;
-    toc.style.marginTop = String(toc_top_margin) + "px";
-  }
+	const content_rect = content.getBoundingClientRect();
+	const content_children = Array.from(content.children);
+	const h1 = content_children.find(is_h1);
+	if (h1) {
+		const h1_rect = h1.getBoundingClientRect();
+		const h1_dy = h1_rect.y - content_rect.y;
+		const toc_top_margin = h1_dy + h1.offsetHeight;
+		toc.style.marginTop = String(toc_top_margin) + "px";
+	}
 };
 
 const on_dom_content_loaded = (function (main_id, content_id) {
-  return function (event) {
-    const main = document.getElementById(main_id);
-    const content = document.getElementById(content_id);
-    const toc = element_toc(content);
-    position_toc(main, content, toc);
-    hljs.highlightAll();
-  };
+	return function (event) {
+		const main = document.getElementById(main_id);
+		const content = document.getElementById(content_id);
+		const toc = element_toc(content);
+		main.appendChild(toc);	  
+		// position_toc(main, content, toc);
+		hljs.highlightAll();
+	};
 })(main_id, content_id);
 
 document.addEventListener("DOMContentLoaded", on_dom_content_loaded);

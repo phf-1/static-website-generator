b7d3438dcc789a815a83bc649e716bb7b70c9945
Pierre-Henry Fröhring <contact@phfrohring.com>
2024-07-10 10:38:15 +0200

    dev: no flickering.

--- js/main.js
+++ js/main.js
@@ -9,20 +9,62 @@ import { Toc } from "./module/toc";
 import { Theorem } from "./module/theorem";
 import { Math } from "./module/math";
 import { Highlight } from "./module/highlight";
+const Container = class {
+	constructor(bg_pic_id) {
+		this.bg_pic_id = bg_pic_id
+	}
+
+	start(container) {
+		this._resize = new ResizeObserver(function(entries) {
+			const rect = entries[0].contentRect;
+			container.style.top = String(rect.height * 0.39) + "px";
+			container.style.visibility = "visible";
+		});
+		this._resize.observe(document.querySelector("#" + this.bg_pic_id));
+	}
+};
+const Root = class {
+	constructor(id) {
+		this.id = id
+	}
+
+	start() {
+		const img = document.getElementById(this.id)
+
+		const show_body = function () {
+			const body = document.getElementsByTagName('body')[0]
+			body.style.visibility = "visible";
+		}
+
+		if (img.complete) {
+			show_body()
+		} else {
+			img.addEventListener('load', show_body)
+			img.addEventListener('error', function() {
+				alert('error')
+			})
+		}
+	}
+};
 
 
 const container_id = "container";
 const content_id = "content";
 const toggle_toc_btn_id = "toggle_toc_btn";
+const bg_pic_id = "bg-image"
+const bg_pic_img_id = "bg-image-img"
 
 
 const theorem = new Theorem();
 const highlight = new Highlight();
 const toc = new Toc();
 const math = new Math();
+const container = new Container(bg_pic_id);
+const root = new Root(bg_pic_img_id);
 
 
 const on_dom_content_loaded = function (event) {
+	container.start(document.getElementById(container_id));
 	const content = document.getElementById(content_id);
 	toc.start(
 		content,
@@ -32,6 +74,7 @@ const on_dom_content_loaded = function (event) {
 	highlight.start(content);
 	theorem.start(content);
 	math.start(content);
+	root.start()
 };
 
 

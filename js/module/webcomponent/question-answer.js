d1b727df29ea1e34f2542d03ef2b9a7936b3e6ff
Pierre-Henry Fröhring <contact@phfrohring.com>
2024-07-13 15:30:14 +0200

    dev: code format.

--- js/module/webcomponent/question-answer.js
+++ js/module/webcomponent/question-answer.js
@@ -2,52 +2,52 @@ import { html, css, LitElement } from "lit";
 import { sharedStyles } from "./style";
 
 class QuestionAnswer extends LitElement {
-	static styles = [
-		sharedStyles,
-		css`
-			.question-answer {
-				margin: var(--x-qa-margin, 4rem) 0;
-			}
-			.question-answer .actions {
-				display: flex;
-				justify-content: right;
-				gap: 1rem;
-			}
-			.question-answer .content {
-				display: flex;
-				flex-direction: column;
-			}
-		`,
-	];
+    static styles = [
+        sharedStyles,
+        css`
+            .question-answer {
+                margin: var(--x-qa-margin, 4rem) 0;
+            }
+            .question-answer .actions {
+                display: flex;
+                justify-content: right;
+                gap: 1rem;
+            }
+            .question-answer .content {
+                display: flex;
+                flex-direction: column;
+            }
+        `,
+    ];
 
-	render() {
-		return html`
-			<div class="question-answer">
-				<div class="content">
-					<slot></slot>
-				</div>
-				<div class="actions">
-					<button class="button" @click="${this._show}">SHOW</button
-					><button class="button" @click="${this._hide}">HIDE</button>
-				</div>
-			</div>
-		`;
-	}
+    render() {
+        return html`
+            <div class="question-answer">
+                <div class="content">
+                    <slot></slot>
+                </div>
+                <div class="actions">
+                    <button class="button" @click="${this._show}">SHOW</button
+                    ><button class="button" @click="${this._hide}">HIDE</button>
+                </div>
+            </div>
+        `;
+    }
 
-	_answers() {
-		return this.shadowRoot
-			.querySelector("slot")
-			.assignedElements()
-			.filter((el) => el.tagName === "X-A");
-	}
+    _answers() {
+        return this.shadowRoot
+            .querySelector("slot")
+            .assignedElements()
+            .filter((el) => el.tagName === "X-A");
+    }
 
-	_show() {
-		this._answers().forEach((answer) => answer.show());
-	}
+    _show() {
+        this._answers().forEach((answer) => answer.show());
+    }
 
-	_hide() {
-		this._answers().forEach((answer) => answer.hide());
-	}
+    _hide() {
+        this._answers().forEach((answer) => answer.hide());
+    }
 }
 
 customElements.define("x-qa", QuestionAnswer);
